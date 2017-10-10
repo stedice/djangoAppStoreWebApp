c93e0e8a24e8de33a16b70f30410a4e70b350827
stedice <stefanodc@mela2.fritz.box>
2017-10-10 17:19:57 +0200

    template

--- appStore/urls.py
+++ appStore/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 	# /appStore
     url(r'^$', views.index, name='index'),
 
-    # /appStore/{$id}/
+    # /appStore/{id}/
     url(r'^(?P<app_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- appStore/views.py
+++ appStore/views.py
@@ -1,20 +1,17 @@
-#from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from django.template import loader
 from .models import App
+
 
 def index(request):
 	all_apps = App.objects.all()
-	html = '<h1>This is the list of available apps</h1>'
-	for app in all_apps:
-		url = '/appstore/' + str(app.id) + '/'
-		html += '<li><a href="' + url + '">' + app.name + '</a></li>'
-	return HttpResponse(html)
-
+	template = loader.get_template('appStore/index.html')
+	context = {
+		'all_apps': all_apps,
+	}
+	return HttpResponse(template.render(context, request))
 
 def detail(request, app_id):
 	return HttpResponse('<h2>Details for app ' + str(app_id) + '</h2>')
 
- 
+   
